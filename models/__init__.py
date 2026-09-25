8d0497df8bea068c12ff7077d5207bfa03c98734
cmh02 <cmh0201@auburn.edu>
2026-09-25 14:14:10 -0500

    Fixing imports

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,9 @@
 """Models module for COMP6970 - QA for Deep Learning."""
 
 from .basic import (
-    BasicModel,
     BasicNeuralNetwork,
-    create_basic_model,
 )
 
 __all__ = [
-    "BasicModel",
     "BasicNeuralNetwork",
-    "create_basic_model",
 ]
